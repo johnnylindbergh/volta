98e7cfbbd0e7bd28389a42a73ceecf4ff7473217
Thomas Castleman <thomas7rex@gmail.com>
2020-12-16 11:07:48 -0500

    implement generation of sentence structures with streams

---
+++ data.js
@@ -0,0 +1,32 @@
+
+class LineStructure {
+  constructor(_sentenceStructs) {
+    this.sentenceStructs = _sentenceStructs;
+  }
+}
+
+class SentenceStructure {
+  constructor(_structure) {
+    this.structure = _structure;
+  }
+}
+
+// class PartOfSpeech {
+//   constructor() {}
+
+//   readWordList() {
+//     this.wordList = [];
+//   }
+// }
+
+// class SingleNoun extends PartOfSpeech {
+//   constructor(_listFile) { this.listFile = _listFile; }
+// }
+
+// class PluralNoun extends PartOfSpeech {
+//   constructor(_listFile) { this.listFile = _listFile; }
+// }
+
+// class ProperNoun extends PartOfSpeech {
+//   constructor(_listFile) { this.listFile = _listFile; }
+// }
